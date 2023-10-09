e33b346a0cb87b15dfa9c47bdce453944677a7e0
sai <rubyer1993@gmail.com>
2023-10-09 09:18:00 +0800

    17. 路由树-参数路径之参数值 完成

---
+++ code/week1/server/16-valueOfParamRoute/context.go
@@ -0,0 +1,13 @@
+package valueOfParamRoute
+
+import "net/http"
+
+// Context HandleFunc的上下文
+type Context struct {
+	// Req 请求
+	Req *http.Request
+	// Resp 响应
+	Resp http.ResponseWriter
+	// PathParams 路径参数名值对
+	PathParams map[string]string
+}
